270f51c314026cd3e33f7d7ea1c8efc4a687d134
BrunoMatosSilva <bmsdevfullstack@gmail.com>
2023-10-22 21:15:34 -0300

    Arquitetura MVC, Removed Pods

---
+++ SwiftArquiteturas/Controllers/HomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  HomeViewController.swift
+//  SwiftArquiteturas
+//
+//  Created by Bruno Matos Silva on 15/10/23.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
